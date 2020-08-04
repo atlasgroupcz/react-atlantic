4d8a3b632cf9bdfb9326eca074e898bb37a74d19
Filip Kaštovský <kastovsky@atlasgroup.cz>
2020-08-04 18:25:17 +0200

    chore: exclude jsx from tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'babel-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  setupFiles: ['./tests/config/setupTests.js'],
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'babel-jest',
+    },
+    setupFiles: ['./tests/config/setupTests.js'],
 };
