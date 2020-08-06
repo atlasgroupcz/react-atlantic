612e1c76d56b825c62ac99f73340ac42e489d8f9
prokop <prokop@atlasgroup.cz>
2020-08-06 09:53:23 +0200

    test(input): mockPropsCheckTest

--- tests/Input.test.tsx
+++ tests/Input.test.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { shallow } from 'enzyme';
 import { InputBase } from '../src/components/Input/view/base';
 import { InputWrapper } from '../src/components/Input/view/composed/withFix';
+import { InputFixProps } from '../src/components/Input/view/composed/withFix/types';
+import { InputProps } from '../src/components/Input/view/base/types';
+
+const mockBaseProps: InputProps = {
+    value: 'value',
+    isRound: true,
+    size: `small`,
+    isDisabled: true,
+};
+
+const mockComposedProps: InputFixProps = {
+    ...mockBaseProps,
+};
+
+const mockPropsCheckTest = <T extends {}>(
+    mockProps: T,
+    Component: FC<T>,
+    findSelector?: string
+) => {
+    Object.keys(mockProps).forEach((key) => {
+        it(`${key} should be round ${mockProps[key]}`, () => {
+            const inputBase = shallow(<Component {...mockProps} />);
+            if (findSelector) {
+                expect(inputBase.find(findSelector)?.props()?.[key]).toEqual(
+                    mockProps[key]
+                );
+            } else {
+                expect(inputBase.props()?.[key]).toEqual(mockProps[key]);
+            }
+        });
+    });
+};
 
 describe('InputBase', () => {
     it('should render InputBase', () => {
         const inputBase = shallow(<InputBase />);
         expect(inputBase).toBeDefined();
     });
+
+    mockPropsCheckTest(mockBaseProps, InputBase);
 });
 
 describe('InputWrapper', () => {
     it('should render InputWrapper', () => {
         const inputWrapper = shallow(<InputWrapper />);
-        expect(inputWrapper).toBeDefined();
+        expect(inputWrapper.find).toBeDefined();
     });
+
+    mockPropsCheckTest(mockComposedProps, InputWrapper, 'InputBase');
 });
