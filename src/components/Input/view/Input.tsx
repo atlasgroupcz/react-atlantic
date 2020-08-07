b1159a688c2f32273681d4a98ff9fbec1418fca1
prokop <prokop@atlasgroup.cz>
2020-08-07 14:07:48 +0200

    refactor(input): forward ref in upper view

--- src/components/Input/view/Input.tsx
+++ src/components/Input/view/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { FC } from 'react';
 import { InputBase } from './base';
 import { InputProps } from './base/types';
@@ -8,11 +8,18 @@ import { InputWrapper as InputWrapperWithFix } from './composed';
 
 export type InputType = FC<PropsWithoutChildren<InputFixProps & InputProps>>;
 
-export const Input: InputType = ({ prefix, suffix, ...props }) => {
-    if (prefix || suffix) {
-        return (
-            <InputWrapperWithFix {...props} prefix={prefix} suffix={suffix} />
-        );
+export const Input: InputType = forwardRef(
+    ({ prefix, suffix, ...props }, ref) => {
+        if (prefix || suffix) {
+            return (
+                <InputWrapperWithFix
+                    {...props}
+                    prefix={prefix}
+                    suffix={suffix}
+                    ref={ref}
+                />
+            );
+        }
+        return <InputBase {...props} ref={ref} />;
     }
-    return <InputBase {...props} />;
-};
+);
