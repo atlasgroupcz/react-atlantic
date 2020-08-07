3e82fa593c48e3ec78c2db691ed55f0a5ac86721
Patrik Szewczyk <szewczyk@atlasgroup.cz>
2020-08-07 12:39:23 +0200

    fix(babel): separated config for jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.tsx?$': 'babel-jest',
+        '^.+\\.ts?$': 'babel-jest',
+        '^.+\\.tsx?$': [
+            'babel-jest',
+            { configFile: './tests/config/.babelrc' },
+        ],
     },
     setupFiles: ['./tests/config/setupTests.js'],
 };
