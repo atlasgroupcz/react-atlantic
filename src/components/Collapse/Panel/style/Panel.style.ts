009d1611c3fca174bddc872815892a49a5460a92
prokop <prokop@atlasgroup.cz>
2020-08-10 11:00:50 +0200

    feat(panel): simple style with flex layout

--- src/components/Collapse/Panel/style/Panel.style.ts
+++ src/components/Collapse/Panel/style/Panel.style.ts
@@ -3,4 +3,8 @@ import {} from '../../../Typography/Text';
 
 export const StyledPanel = styled.div``;
 export const StyledPanelContent = styled.div``;
-export const StyledPanelHeader = styled.div``;
+export const StyledPanelHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
