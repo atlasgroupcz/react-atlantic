bd31213099134312549e06feb84aa4ac63a37d18
prokop <prokop@atlasgroup.cz>
2020-08-11 14:59:28 +0200

    refactor(collapse): demo comment deleted

--- src/components/Collapse/demo.tsx
+++ src/components/Collapse/demo.tsx
@@ -9,22 +9,6 @@ export const Demo = (): StoryFnReactReturnType => {
         isAccordion: false,
     });
 
-    // const firstDemo = wrap(
-    //     Collapse,
-    //     collapseUniqueState,
-    //     <>
-    //         <Collapse.Panel header="level1 - 1" unique="1">
-    //             <p>prdel 1</p>
-    //         </Collapse.Panel>
-    //         <Collapse.Panel header="level1 - 2" unique="2">
-    //             <p>prdel 2</p>
-    //         </Collapse.Panel>
-    //         <Collapse.Panel header="level1 - 3" unique="3">
-    //             <p>prdel 3</p>
-    //         </Collapse.Panel>
-    //     </>
-    // );
-
     const secondDemo = wrapCurried(Collapse)(
         collapseUniqueState,
         <>
