46d2bf31346b8f8245af58db9cdc70e1da0a86aa
prokop <prokop@atlasgroup.cz>
2020-08-25 16:01:04 +0200

    refactor(timeline): ♻️ deleted hooks folder

--- src/components/Timeline/hooks/useTimelineActive/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useTimelineActive';
-export * from './useTimelineActive.types';
--- src/components/Timeline/hooks/useTimelineActive/useTimelineActive.types.ts
+++
@@ -1,8 +0,0 @@
-import { TimelineItemProps } from '../../view/Item/Item.types';
-
-export type UseTimelineActiveType = (
-    props: UseTimelineActiveProps
-) => UseTimelineActiveValue;
-
-type UseTimelineActiveProps = TimelineItemProps;
-type UseTimelineActiveValue = TimelineItemProps;
