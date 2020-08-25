a8806fe86e3af6462b0d25490048c3dc671756fa
prokop <prokop@atlasgroup.cz>
2020-08-25 15:56:07 +0200

    refactor(timeline): ♻️ deleted old styles

--- src/components/Timeline/view/Item/style/Item.style.tsx
+++ src/components/Timeline/view/Item/style/Item.style.tsx
@@ -1,21 +1,14 @@
 import React, { forwardRef, PropsWithChildren } from 'react';
-import { css, Type, PureButton, MouseEvents, StyledIcon } from '../../../../..';
-import { StyledTitle5 } from '../../../../Typography/Title/Title.style';
-import { StyledText } from '../../../../Typography/Text/Text.style';
+import { css, Type, MouseEvents } from '../../../../..';
+
 import { styled } from '../../../../../styled';
 import { TimelineItemProps } from '../Item.types';
-import { Link } from '../../../../Typography/Link';
 
 const circleSize = `15px`;
 
 export interface StyledTimelineCircleProps {
     isActive?: boolean;
     type?: Type;
-    isDisabled?: Readonly<boolean>;
-}
-
-export interface StyledTimelineTitleProps {
-    isActive?: boolean;
     isDisabled?: Readonly<boolean>;
 }
 
@@ -102,96 +95,6 @@ export const StyledTimelineCircle = styled.div<StyledTimelineCircleProps>`
   
 `;
 
-export const StyledTimelineTitle = styled((props) => <Link {...props} />)<
-    StyledTimelineTitleProps
->`
-    display: block;
-    margin: 0;
-    font-weight: 600;
-    color: ${(props) => props.theme.color.text.alpha};
-
-    ${(props) =>
-        !props.isDisabled &&
-        css`
-            &:hover {
-                color: ${(props) => props.theme.color.primary.alpha};
-
-                ${StyledTitle5} {
-                    color: ${(props) => props.theme.color.primary.alpha};
-                }
-            }
-        `}
-
-    ${StyledTitle5} {
-        margin: 0;
-        color: ${(props) => props.theme.color.text.alpha};
-
-        ${(props) =>
-            props.isActive &&
-            css`
-                color: ${(props) => props.theme.color.primary.alpha};
-            `}
-    }
-
-    ${(props) =>
-        props.isDisabled &&
-        css`
-            &:hover {
-                text-decoration: none;
-            }
-
-            ${StyledTitle5} {
-                color: ${props.theme.color.text.beta};
-            }
-
-            cursor: not-allowed;
-        `}
-`;
-
-export const StyledTimelineCaption = styled((props) => <Link {...props} />)<{
-    hasMarginRight: Readonly<boolean>;
-    isDisabled?: Readonly<boolean>;
-}>`
-    display: block;
-    margin: 0;
-    color: ${(props) => props.theme.color.text.beta};
-    font-size: ${(props) => props.theme.font.size.sm};
-
-    ${(props) =>
-        props.hasMarginRight &&
-        css`
-            margin-right: ${props.theme.margin.sm};
-        `}
-
-    ${StyledText} {
-        color: ${(props) => props.theme.color.text.beta};
-    }
-
-    ${(props) =>
-        props.isDisabled &&
-        css`
-            cursor: not-allowed;
-        `}
-`;
-
 export const StyledTimelineContainer = styled.div<StyledTimelineContainerProps>`
     flex: 1 1 0;
-`;
-
-export const StyledTimelineButton = styled((props) => (
-    <PureButton {...props} />
-))`
-    ${StyledIcon} {
-        height: auto;
-    }
-`;
-
-export const StyledTimelineCaptionContainer = styled.div`
-    display: flex;
-`;
-
-export const StyledListElementsContainer = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
 `;
