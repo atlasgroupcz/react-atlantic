d8d045089e199d320b4ce2518378f7d47d44814b
prokop <prokop@atlasgroup.cz>
2020-09-10 10:52:51 +0200

    test: 🚨 pagination wip

--- tests/suites/components/Pagination.test.tsx
+++ tests/suites/components/Pagination.test.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { PaginationProps, Pagination } from '../../../src/components';
 import { toBeDefinedTest } from '../../shared';
+import { useState } from 'react';
+import { wrap } from '../../../src/utils';
 
 const mockPaginationProps: PaginationProps = {
     total: 30,
@@ -7,8 +10,26 @@ const mockPaginationProps: PaginationProps = {
     page: 1,
 };
 
+//TODO!: after merge timeline please finish tests
 describe('Pagination', () => {
     toBeDefinedTest(Pagination, mockPaginationProps);
     // mockPropsCheckTest(mockPaginationProps, Pagination);
     // mountTest(Pagination);
+
+    it('button should be own defined sequence', () => {});
+    it('should has own prev and next buttons', () => {});
+    it('should change has correct structure', () => {});
+    it('should change structure change page', () => {});
 });
+
+type TestLogicHookType = (props: PaginationProps) => PaginationProps;
+const useTestLogicHook: TestLogicHookType = ({ ...props }) => {
+    const [page, setPage] = useState<number>(1);
+
+    const handleClick: PaginationProps['onClick'] = (e, p) => {
+        setPage(p);
+    };
+    return { page, onClick: handleClick, ...props };
+};
+
+const TestPaginationWithLogic = wrap(Pagination, useTestLogicHook);
