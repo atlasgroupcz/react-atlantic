ed31f11f01342d958179e0bc686f3b530596a270
prokop <prokop@atlasgroup.cz>
2020-09-17 15:37:19 +0200

    test(tabs): 🚨 wip

--- tests/suites/components/Tabs.test.tsx
+++ tests/suites/components/Tabs.test.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { PaginationProps, TabsProps, Tabs } from '../../../src/components';
-import { toBeDefinedTest, mountTest } from '../../shared';
-import { useState } from 'react';
+import { TabsProps, Tabs } from '../../../src/components';
+import { mountTest, toBeDefinedTest } from '../../shared';
 
-const mockTabsProps: TabsProps = {
-    List: () => <>LIST</>,
-    activeKey: '1',
+export const TmpList = () => {
+    return <>TMP LIST</>;
 };
 
-type TestLogicHookType = (props: PaginationProps) => PaginationProps;
-const useTestLogicHook: TestLogicHookType = ({ ...props }) => {
-    const [page, setPage] = useState<number>(1);
-
-    const handleClick: PaginationProps['onClick'] = (e, p) => {
-        setPage(p);
-    };
-    return { page: page, onClick: handleClick, ...props };
+const mockTabsProps: TabsProps = {
+    activeKey: '1',
+    List: TmpList,
 };
 
 describe('Tabs', () => {
