e09b3ca5913636f1aa411f2b7abc4e8089733392
prokop <prokop@atlasgroup.cz>
2020-09-23 18:11:26 +0200

    refactor: ♻️ demo

--- stories/DatePicker.story.tsx
+++ stories/DatePicker.story.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { DatePickerFirstDemo } from './demos/DatePicker';
-import { DatePicker } from '../src';
 import { StyledStory } from './style';
 
 const stories = storiesOf('DatePicker', module);
