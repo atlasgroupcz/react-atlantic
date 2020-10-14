f20539fd4a95f7190ba6b10118473b6ca7b1e07b
nikolasrnickova <nikola.srnickova@gmail.com>
2020-10-14 14:55:32 +0200

    style: 🎨 simpleButton

--- src/components/Button/view/index.ts
+++ src/components/Button/view/index.ts
@@ -1,2 +1,3 @@
-export * from './default/Button';
-export * from './default/style';
+export * from './default';
+export * from './pure';
+export * from './simple';
--- src/components/Button/view/simple/SimpleButton.tsx
+++ src/components/Button/view/simple/SimpleButton.tsx
@@ -1,10 +1,90 @@
-import styled from 'styled-components';
-import { PureButton } from '../pure';
+import styled, { css } from 'styled-components';
+import { StyledIcon } from '../../../Icon';
+import { StyledText } from '../../../Typography/Text/Text.style';
+import { ButtonProps } from '../../types';
+import { Button } from '../default';
 
-export const SimpleButton = styled(PureButton)`
+export type SimpleButtonProps = {
+    isAlternative?: boolean;
+} & ButtonProps;
+
+export const SimpleButton = styled(Button)<SimpleButtonProps>`
     && {
-        :hover {
-            background: ${props.theme.color.background.alpha};
+        background: ${(props) => props.theme.color.background.alpha};
+        box-shadow: none;
+        border: none;
+
+        &:hover {
+            background: ${(props) => props.theme.color.background.beta};
         }
+
+        ${(props) =>
+            props.isAlternative &&
+            css`
+                background: ${props.theme.color.background.beta};
+
+                &:hover {
+                    background: ${props.theme.color.background.alpha};
+                }
+            `}
+
+        ${(props) =>
+            props.type &&
+            css`
+                ${props.type === 'primary' &&
+                css`
+                    background: ${props.theme.color.primary.alpha};
+
+                    &:hover {
+                        background: ${props.theme.color.primary.beta};
+                    }
+                `}
+
+                ${props.type === 'success' &&
+                css`
+                    background: ${props.theme.color.success.alpha};
+
+                    &:hover {
+                        background: ${props.theme.color.success.beta};
+                    }
+                `}
+          
+          ${props.type === 'warning' &&
+                css`
+                    background: ${props.theme.color.warning.alpha};
+
+                    &:hover {
+                        background: ${props.theme.color.warning.beta};
+                    }
+                `}
+          
+          ${props.type === 'error' &&
+                css`
+                    background: ${props.theme.color.error.alpha};
+
+                    &:hover {
+                        background: ${props.theme.color.error.beta};
+                    }
+                `}
+          
+          ${props.type === 'dashed' &&
+                css`
+                    border: 1px dashed ${props.theme.color.border};
+                `}
+            `}
+
+        ${(props) =>
+            props.isDisabled &&
+            css`
+                background: transparent;
+
+                &:hover {
+                    background: transparent;
+                }
+
+                ${StyledText}, ${StyledIcon} {
+                    opacity: 0.6;
+                }
+            `}
     }
 `;
