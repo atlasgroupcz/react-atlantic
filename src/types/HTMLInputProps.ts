3e0f94c8e307b6ca07554ebe9ad84435622e8a9f
Filip Kaštovský <kastovsky@atlasgroup.cz>
2020-10-26 09:52:37 +0100

    fix: 🐛 HTMLInput multiple files prop

--- src/types/HTMLInputProps.ts
+++ src/types/HTMLInputProps.ts
@@ -2,7 +2,7 @@ import { ElementProps } from './utils';
 
 export type HTMLInputElementProps = Pick<
     ElementProps<HTMLInputElement>,
-    'value' | 'readOnly' | 'name' | 'accept'
+    'value' | 'readOnly' | 'name' | 'accept' | 'multiple'
 >;
 
 export type HTMLInputDefaultElementProps = Pick<
