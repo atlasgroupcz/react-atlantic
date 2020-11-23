5fd97e115c2782c70afd04c8b40d9ac9f25943ef
Filip Kaštovský <devfilipkastovsky@gmail.com>
2020-11-23 16:00:04 +0100

    feat: ✨ tooltps

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -3,10 +3,11 @@ import styledStyled, {
     keyframes as styledKeyframes,
     ThemedStyledInterface,
     createGlobalStyle as styledCreateGlobalStyle,
+    ThemedCssFunction,
 } from 'styled-components';
 import { ThemeType } from '../theme';
 
-export const css = styledCss;
+export const css = styledCss as ThemedCssFunction<ThemeType>;
 export const createGlobalStyle = styledCreateGlobalStyle;
 export const keyframes = styledKeyframes;
 export const styled = styledStyled as ThemedStyledInterface<ThemeType>;
