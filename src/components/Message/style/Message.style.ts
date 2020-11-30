58117c1d5a9da7c6e123e12ddc46cfff3bab45db
nikolasrnickova <nikola.srnickova@gmail.com>
2020-11-30 13:14:42 +0100

    style: 🎨 remove unused styled component

--- src/components/Message/style/Message.style.ts
+++ src/components/Message/style/Message.style.ts
@@ -65,20 +65,18 @@ export const StyledMessageIcon = styled(Icon)<StyledMessageProps>`
     margin-right: ${(props) => props.theme.padding.md};
 
     ${(props) =>
-        props.name === 'loading' &&
-        css`
-            width: 14px;
-            height: 14px;
-            margin: 0;
-        `}
-
-    ${(props) =>
         !props.isTransparent &&
         css`
             color: ${props.theme.color.text.gamma};
         `}
 
     ${(props) =>
+        props.type === 'default' &&
+        css`
+            color: ${props.theme.color.text.alpha};
+        `}
+
+    ${(props) =>
         props.type === 'primary' &&
         props.isTransparent &&
         css`
@@ -88,68 +86,24 @@ export const StyledMessageIcon = styled(Icon)<StyledMessageProps>`
   ${(props) =>
         props.type === 'success' &&
         props.isTransparent &&
-        css`
-            && {
-                color: ${props.theme.color.success.alpha};
-            }
-        `}
-  
-  ${(props) =>
-        props.type === 'warning' &&
-        props.isTransparent &&
-        css`
-            color: ${props.theme.color.warning.alpha};
-        `}
-  
-  ${(props) =>
-        props.type === 'error' &&
-        props.isTransparent &&
-        css`
-            color: ${props.theme.color.error.alpha};
-        `}
-`;
-
-export const StyledMessageIconLoading = styled(Icon)<StyledMessageProps>`
-    cursor: default;
-    position: absolute;
-    top: 13px;
-    left: 10px;
-
-    ${(props) =>
-        props.name === 'loading' &&
-        css`
-            width: 14px;
-            height: 14px;
-            margin: 0;
-        `}
-
-    ${(props) =>
-        props.type === 'primary' &&
-        css`
-            color: ${props.theme.color.primary.alpha};
-        `}
-  
-  ${(props) =>
-        props.type === 'success' &&
         css`
             color: ${props.theme.color.success.alpha};
         `}
   
   ${(props) =>
         props.type === 'warning' &&
+        props.isTransparent &&
         css`
             color: ${props.theme.color.warning.alpha};
         `}
   
   ${(props) =>
         props.type === 'error' &&
+        props.isTransparent &&
         css`
             color: ${props.theme.color.error.alpha};
         `}
 `;
-export const StyledMessageContentSpan = styled.span<StyledMessageProps>``;
 
 StyledMessageContainer.displayName = 'StyledMessageContainer';
 StyledMessageIcon.displayName = 'StyledMessageIcon';
-StyledMessageIconLoading.displayName = 'StyledMessageIconLoading';
-StyledMessageContentSpan.displayName = 'StyledMessageContentSpan';
