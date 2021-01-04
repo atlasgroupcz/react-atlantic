a8d9ffdf50daa76273dd9f83fd166eec87841e65
nikolasrnickova <nikola.srnickova@gmail.com>
2021-01-04 09:59:02 +0100

    style: 🎨 add styledCollapse

--- src/components/Collapse/style/Collapse.style.ts
+++ src/components/Collapse/style/Collapse.style.ts
@@ -1,5 +1,7 @@
 import { styled } from '../../../styled';
 
+export const StyledCollapse = styled.div``;
+
 export const StyledCollapseIconContainer = styled.div`
     display: flex;
     align-items: center;
