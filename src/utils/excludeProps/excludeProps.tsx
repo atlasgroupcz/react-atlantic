5ceb94ba121e7a5414bff2767eabe85ca3cba109
Filip Kaštovský <devfilipkastovsky@gmail.com>
2021-02-03 19:18:32 +0100

    feat!: 💥 ✨ overload excludeProps

--- src/utils/excludeProps/excludeProps.tsx
+++ src/utils/excludeProps/excludeProps.tsx
@@ -1,37 +1,14 @@
-import React, {
-    ComponentPropsWithoutRef,
-    createElement,
-    FC,
-    memo,
-} from 'react';
+import { ComponentPropsWithoutRef, createElement, FC, memo } from 'react';
 import { removeProperties } from '../removeProperties';
 
-export const excludeComponentProps = <R extends {} = {}>() => <
-    T extends FC<any>,
-    E extends ReadonlyArray<keyof ComponentPropsWithoutRef<T> | keyof R>
->(
-    Component: T,
-    excludeArr: E
-): FC<ComponentPropsWithoutRef<T> & R> =>
-    memo(({ children, ...props }) => {
-        const AnyComponent = Component as any;
-        return (
-            <AnyComponent {...removeProperties(props, excludeArr as any)}>
-                {children}
-            </AnyComponent>
-        );
-    });
-
-export const excludeIntrinsicElementProps = <R extends {} = {}>() => <
-    T extends keyof JSX.IntrinsicElements,
-    A extends FC<any>,
-    E extends ReadonlyArray<keyof JSX.IntrinsicElements[T] | keyof R>
+export const excludeProps = <R extends {}>() => <
+    T extends keyof JSX.IntrinsicElements | FC<any>,
+    E extends ReadonlyArray<keyof R>,
+    As extends keyof JSX.IntrinsicElements | FC<any> | undefined
 >(
     element: T,
     excludeArr: E
-): FC<
-    ComponentPropsWithoutRef<T> & R & { as?: keyof JSX.IntrinsicElements | A }
-> =>
+): FC<ComponentPropsWithoutRef<T> & R & { as?: As }> =>
     memo(({ children, as, ...props }) => {
         return createElement(
             as ?? element,
