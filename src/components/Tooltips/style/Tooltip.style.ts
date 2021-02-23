2d9323de64ec95397538d1dff907116987f68cc4
nikolasrnickova <nikola.srnickova@gmail.com>
2021-02-23 09:11:39 +0100

    style: 🎨 styledTooltip

--- src/components/Tooltips/style/Tooltip.style.ts
+++ src/components/Tooltips/style/Tooltip.style.ts
@@ -1,6 +1,18 @@
 import { styled } from '../../../styled';
 
-export const Tooltip = styled.div`
+export const StyledTooltip = styled.div`
+    z-index: 1000;
     position: fixed;
-    pointer-events: none;
+    padding: 0 ${(props) => props.theme.padding.md};
+    font-size: ${(props) => props.theme.font.size.md};
+    font-family: ${(props) => props.theme.font.family};
+    line-height: 2;
+    color: ${(props) => props.theme.color.text.gamma};
+    background: ${(props) => props.theme.color.background.epsilon + '99'};
+    border-radius: ${(props) => props.theme.radius};
+`;
+
+export const StyledTooltipContainer = styled.div`
+    position: relative;
+    display: inline-block;
 `;
