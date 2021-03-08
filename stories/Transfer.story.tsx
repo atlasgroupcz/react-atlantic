086be7435ff86fc2329580d9362ca1b71a18b2ea
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-03-08 10:19:13 +0100

    chore: 🔧 types

--- stories/Transfer.story.tsx
+++ stories/Transfer.story.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import { Transfer } from '../src/components/Transfer';
