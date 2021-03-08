be50982f07b9551296347cd6221345fe752f25d9
Filip Kaštovský <kastovsky@atlasgroup.cz>
2021-03-08 09:57:56 +0100

    feat: ✨ js tooltips

--- stories/Tooltips.story.tsx
+++ stories/Tooltips.story.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { Text, Tooltips } from '../src';
 
@@ -7,18 +7,22 @@ const stories = storiesOf('Tooltips', module);
 stories.add(
     'Playground',
     () => {
-        const [state] = useState<number>(0);
+        const [state, setState] = useState<number>(0);
+
+        useEffect(() => {
+            setInterval(() => {
+                setState((s) => s + 1);
+            }, 1000);
+        }, []);
 
         return (
             <>
                 <Tooltips>
                     <div>
-                        {!!(state % 5) && (
-                            <Text data-title={state}>Hello world! {state}</Text>
-                        )}
+                        {<Text data-title={state}>Hello world! {state}</Text>}
                         <br />
                         <br />
-                        {[...new Array(10)].map((_, i) => (
+                        {[...new Array(1)].map((_, i) => (
                             <Text key={i}>
                                 Custom position: (
                                 <Text data-title="left" data-position="left">
@@ -63,21 +67,18 @@ stories.add(
                         </Text>
                     </div>
                 </Tooltips>
-                {/* <br />
+                <br />
                 <Tooltips contentAttr="data-custom">
                     <Text data-custom="Custom attributes!">
                         Custom attributes!
                     </Text>
                 </Tooltips>
                 <br />
-                <Tooltips transition="opacity: 1;transition: all 1s;">
+                <Tooltips transition="opacity: 1;transition: all 1s; ">
                     <Text data-title="Custom transition!">
                         Custom transition!
                     </Text>
                 </Tooltips>
-                <Tooltips transition="opacity: 1;transition: all 1s;">
-                    <Text data-title="Custom transition!">Custom Tooltip!</Text>
-                </Tooltips> */}
             </>
         );
     },
