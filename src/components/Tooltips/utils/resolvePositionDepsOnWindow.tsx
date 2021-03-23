e3610a320a8eb66f9ba1cca027516aca2c8e77a4
prokop <prokop@atlasgroup.cz>
2021-03-23 12:59:12 +0100

    chore: 🔧 comments

--- src/components/Tooltips/utils/resolvePositionDepsOnWindow.tsx
+++ src/components/Tooltips/utils/resolvePositionDepsOnWindow.tsx
@@ -8,21 +8,15 @@ export const resolvePositionDepsOnWindow = (
     switch (preferredDirection) {
         case 'left':
             return left - tooltipElement.clientWidth < 0 ? 'right' : 'left';
-        //if fit else return right
         case 'right':
             return right + tooltipElement.clientWidth > window.innerWidth
                 ? 'left'
                 : 'right';
-
-        //if fit else return left
         case 'bottom':
             return bottom + tooltipElement.clientHeight > window.innerHeight
                 ? 'top'
                 : 'bottom';
-        //if fit else return top
         case 'top':
             return top - tooltipElement.clientHeight < 0 ? 'bottom' : 'top';
-
-        //if fit else return bottom
     }
 };
