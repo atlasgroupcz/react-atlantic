c705c7bd63f9ce00ac457ec8c8b1d7dd6be73b7c
prokop <prokop@atlasgroup.cz>
2021-03-24 08:14:39 +0100

    fix: 🐛 tooltips margin

--- src/components/Tooltips/styles/Tooltip.style.ts
+++ src/components/Tooltips/styles/Tooltip.style.ts
@@ -3,19 +3,18 @@ import { css, styled } from '../../../styled';
 export const tooltipStyles = css<{ positionAttr: string }>`
     opacity: 0;
     visibility: hidden;
-
     z-index: 1000;
     position: fixed;
-
     padding: ${(props) => props.theme.padding.sm}
         ${(props) => props.theme.padding.md};
     font-size: ${(props) => props.theme.font.size.md};
     font-family: ${(props) => props.theme.font.family};
     line-height: 1.5;
     color: ${(props) => props.theme.color.text.gamma};
-    background: ${(props) => props.theme.color.background.epsilon + 'dd'};
     border-radius: ${(props) => props.theme.radius};
     pointer-events: none;
+    backdrop-filter: blur(2px);
+    background-color: rgba(0, 0, 0, 0.65);
 `;
 
 export const StyledTooltip = styled.div<{ positionAttr: string }>`
