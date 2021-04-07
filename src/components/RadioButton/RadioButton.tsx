eb44ff4db4faa3a289484ea46b7b9ea494654efe
Patrik Meixner <meixner@atlasgroup.cz>
2021-04-07 13:51:07 +0200

    chore(radio-button): 🔧 component

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
-import { wrap } from '../../utils';
-import { UseRadioButtonsProps, useRadioButtons } from './hooks';
+import { UseRadioButtonsProps } from './hooks';
 import { RadioButtonsView } from './view';
 
 export type RadioButtonsType = FC<UseRadioButtonsProps>;
 
-export const RadioButtons: RadioButtonsType = wrap(
-    RadioButtonsView,
-    useRadioButtons
-);
+export const RadioButtons: RadioButtonsType = RadioButtonsView;
