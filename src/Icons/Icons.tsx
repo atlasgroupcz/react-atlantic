d9585c383a44abb718a30aed50be8df937f92c65
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-04-13 20:07:59 +0200

    fix(checkbox): 🐛 animation removed

--- src/Icons/Icons.tsx
+++ src/Icons/Icons.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { animated } from 'react-spring';
 import { styled } from '../styled';
 
-const StyledSVG = styled(animated.svg)``;
+const StyledSVG = styled.svg``;
 
 export const ArrowUp = () => (
     <svg
@@ -213,28 +212,6 @@ export const Copy = () => (
             d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"
         />
     </svg>
-);
-
-export const CheckSimple = (props: any) => (
-    <StyledSVG
-        width="14px"
-        height="14px"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="xMidYMid"
-        viewBox="0 0 128 128"
-        fill="none"
-        stroke="currentColor"
-    >
-        <animated.path
-            strokeLinecap={'round'}
-            strokeLinejoin={'round'}
-            strokeDasharray={100}
-            strokeDashoffset={props.x}
-            pathLength="100"
-            strokeWidth="25"
-            d="M24.75 62l27.5 27.5 51-51"
-        />
-    </StyledSVG>
 );
 
 export const Check = () => (
