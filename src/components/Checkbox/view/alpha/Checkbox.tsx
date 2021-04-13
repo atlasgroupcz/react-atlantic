136967182dfd8c1260e5f5ba47b3f61715f2e10b
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-04-13 20:07:59 +0200

    fix(checkbox): 🐛 animation removed

--- src/components/Checkbox/view/alpha/Checkbox.tsx
+++ src/components/Checkbox/view/alpha/Checkbox.tsx
@@ -1,5 +1,4 @@
-import React, { useRef } from 'react';
-import { CheckSimple } from '../../../../Icons';
+import React from 'react';
 import {
     HiddenCheckbox,
     StyledCheckboxIcon,
@@ -8,8 +7,6 @@ import {
     StyledCheckboxMark,
     StyledCheckboxSpan,
 } from './styles';
-import { useChain, useSpring, useTrail } from 'react-spring';
-import * as easings from 'd3-ease';
 import { CheckboxType } from '../../types';
 
 export const Checkbox: CheckboxType = ({
@@ -22,80 +19,55 @@ export const Checkbox: CheckboxType = ({
     className,
     size = 'small',
     ...props
-}) => {
-    const springRef = useRef(null);
-    const springProps = useSpring({
-        ref: springRef,
-        x: isChecked ? 0 : 100,
-        config: { easing: (t) => easings.easeCubicIn(t), duration: 125 },
-    });
+}) => (
+    <StyledCheckboxLabel
+        isChecked={isChecked}
+        isDisabled={isDisabled}
+        className={className}
+        htmlFor={id}
+        size={size}
+    >
+        {horizontalPosition === 'right' && children && (
+            <StyledCheckboxSpan
+                position={horizontalPosition}
+                isDisabled={isDisabled}
+                size={size}
+            >
+                {children}
+            </StyledCheckboxSpan>
+        )}
 
-    const trailRef = useRef(null);
-    const trail = useTrail(1, {
-        ref: trailRef,
-        opacity: isPartiallyChecked || isChecked ? 1 : 0,
-        config: { duration: 125 },
-    });
+        <HiddenCheckbox
+            id={id}
+            readOnly
+            defaultChecked={isChecked}
+            disabled={isDisabled}
+            {...props}
+        />
 
-    useChain(isChecked ? [trailRef, springRef] : [springRef, trailRef], [
-        0,
-        isChecked ? 0.075 : 0.225,
-    ]);
-
-    return (
-        <StyledCheckboxLabel
-            isChecked={isChecked}
-            isDisabled={isDisabled}
-            className={className}
-            htmlFor={id}
-            size={size}
-        >
-            {horizontalPosition === 'right' && children && (
-                <StyledCheckboxSpan
-                    position={horizontalPosition}
+        <StyledCheckboxInputShown isDisabled={isDisabled}>
+            <StyledCheckboxMark
+                isDisabled={isDisabled}
+                isChecked={isChecked}
+                isPartiallyChecked={isPartiallyChecked}
+            >
+                <StyledCheckboxIcon
+                    name={`check`}
                     isDisabled={isDisabled}
-                    size={size}
-                >
-                    {children}
-                </StyledCheckboxSpan>
-            )}
+                    isChecked={isChecked}
+                    isPartiallyChecked={isPartiallyChecked}
+                />
+            </StyledCheckboxMark>
+        </StyledCheckboxInputShown>
 
-            <HiddenCheckbox
-                id={id}
-                readOnly
-                defaultChecked={isChecked}
-                disabled={isDisabled}
-                {...props}
-            />
-            <StyledCheckboxInputShown isDisabled={isDisabled}>
-                {trail.map((style: unknown, index) => (
-                    <StyledCheckboxMark
-                        key={index}
-                        style={style as any}
-                        isDisabled={isDisabled}
-                        isChecked={isChecked}
-                        isPartiallyChecked={isPartiallyChecked}
-                    >
-                        <StyledCheckboxIcon
-                            isDisabled={isDisabled}
-                            isChecked={isChecked}
-                            isPartiallyChecked={isPartiallyChecked}
-                        >
-                            <CheckSimple x={springProps.x} />
-                        </StyledCheckboxIcon>
-                    </StyledCheckboxMark>
-                ))}
-            </StyledCheckboxInputShown>
-
-            {horizontalPosition === 'left' && children && (
-                <StyledCheckboxSpan
-                    position={horizontalPosition}
-                    isDisabled={isDisabled}
-                    size={size}
-                >
-                    {children}
-                </StyledCheckboxSpan>
-            )}
-        </StyledCheckboxLabel>
-    );
-};
+        {horizontalPosition === 'left' && children && (
+            <StyledCheckboxSpan
+                position={horizontalPosition}
+                isDisabled={isDisabled}
+                size={size}
+            >
+                {children}
+            </StyledCheckboxSpan>
+        )}
+    </StyledCheckboxLabel>
+);
