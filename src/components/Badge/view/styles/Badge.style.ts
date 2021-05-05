a4dceade5d700458ba031bd44cf46c87de225585
nikolasrnickova <nikola.srnickova@gmail.com>
2021-05-05 14:03:37 +0200

    refactor: ♻️ badge constants

--- src/components/Badge/view/styles/Badge.style.ts
+++ src/components/Badge/view/styles/Badge.style.ts
@@ -1,9 +1,7 @@
 import styled, { css } from 'styled-components';
 import { BadgeProps } from '../../types';
 
-export const StyledBadge = styled.sup.withConfig({
-    shouldForwardProp: (prop) => !['size', 'type', 'color'].includes(prop),
-})<BadgeProps>`
+export const getBadgeDefaultStyles = () => css`
     display: inline-block;
     text-align: center;
     color: ${(props) => props.theme.color.text.alpha};
@@ -11,56 +9,70 @@ export const StyledBadge = styled.sup.withConfig({
     font-weight: 400;
     background-color: ${(props) => props.theme.color.default};
     border-radius: ${(props) => props.theme.radius};
+`;
 
+export const getBadgeTypeStyles = (type: BadgeProps) => css`
     ${(props) =>
-        props.type === 'primary' &&
+        type === 'primary' &&
         css`
             background-color: ${props.theme.color.primary.alpha};
             color: ${props.theme.color.text.gamma};
         `}
 
     ${(props) =>
-        props.type === 'success' &&
+        type === 'success' &&
         css`
             background-color: ${props.theme.color.success.alpha};
             color: ${props.theme.color.text.gamma};
         `}
-    
-  ${(props) =>
-        props.type === 'warning' &&
+
+    ${(props) =>
+        type === 'warning' &&
         css`
             background-color: ${props.theme.color.warning.alpha};
             color: ${props.theme.color.text.gamma};
         `}
-    
-  ${(props) =>
-        props.type === 'error' &&
+
+    ${(props) =>
+        type === 'error' &&
         css`
             background-color: ${props.theme.color.error.alpha};
             color: ${props.theme.color.text.gamma};
         `}
+`;
 
-  ${(props) =>
-        props.size === 'small' &&
+export const getBadgeSizeStyles = (size: BadgeProps) => css`
+    ${(props) =>
+        size === 'small' &&
         css`
             padding: 2px ${props.theme.padding.sm};
 
             font-size: ${props.theme.font.size.sm};
         `}
-  
+
     ${(props) =>
-        props.size === 'medium' &&
+        size === 'medium' &&
         css`
             padding: ${props.theme.padding.sm}
                 ${parseInt(props.theme.padding.md, 0) - 2}px;
             font-size: ${props.theme.font.size.md};
         `}
     
-      ${(props) =>
-        props.size === 'large' &&
+    ${(props) =>
+        size === 'large' &&
         css`
             padding: ${props.theme.padding.md}
                 ${parseInt(props.theme.padding.lg, 0) - 2}px;
             font-size: ${props.theme.font.size.lg};
         `}
+`;
+
+export const StyledBadge = styled.sup.withConfig({
+    shouldForwardProp: (prop) => !['size', 'type', 'color'].includes(prop),
+})<BadgeProps>`
+    ${getBadgeDefaultStyles()};
+
+    ${(props) => getBadgeTypeStyles(props.type as BadgeProps)};
+
+    ${(props) => getBadgeSizeStyles(props.size as BadgeProps)};
 `;
