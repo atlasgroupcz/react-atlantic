bd94b9cb2e347c6d8e52b9ce8daa1fe3f2b4bc0a
Martin Topolanek <topolanekmar@gmail.com>
2021-05-10 16:58:01 +0200

    refactor(indicator): ♻️ component styles separated into public functions

--- src/components/Indicator/view/styles/Indicator.style.ts
+++ src/components/Indicator/view/styles/Indicator.style.ts
@@ -1,57 +1,73 @@
 import styled, { css } from 'styled-components';
-import { IndicatorProps } from '../../types/Indicator.types';
+import { IndicatorProps } from '../../types';
+
+export const getIndicatorDefaultStyles = () => css`
+    display: block;
+    background-color: ${({ theme }) => theme.color.default};
+`;
+
+export const getIndicatorDefaultTypeStyles = (
+    type: IndicatorProps['type']
+) => css`
+    ${({ theme }) =>
+        type === 'primary' &&
+        css`
+            background-color: ${theme.color.primary.alpha};
+        `}
+
+    ${({ theme }) =>
+        type === 'success' &&
+        css`
+            background-color: ${theme.color.success.alpha};
+        `}
+    
+  ${({ theme }) =>
+        type === 'warning' &&
+        css`
+            background-color: ${theme.color.warning.alpha};
+        `}
+    
+  ${({ theme }) =>
+        type === 'error' &&
+        css`
+            background-color: ${theme.color.error.alpha};
+        `}
+`;
+
+export const getIndicatorDefaultSizeStyles = (
+    size: IndicatorProps['size']
+) => css`
+    ${({ theme }) =>
+        size === 'small' &&
+        css`
+            width: ${theme.padding.sm};
+            height: ${theme.padding.sm};
+            border-radius: ${theme.padding.sm};
+        `}
+
+    ${({ theme }) =>
+        size === 'medium' &&
+        css`
+            width: ${theme.padding.md};
+            height: ${theme.padding.md};
+            border-radius: ${theme.padding.md};
+        `}
+    
+  ${({ theme }) =>
+        size === 'large' &&
+        css`
+            width: ${theme.padding.lg};
+            height: ${theme.padding.lg};
+            border-radius: ${theme.padding.lg};
+        `}
+`;
 
 export const StyledIndicator = styled.sup.withConfig({
     shouldForwardProp: (prop) => !['size', 'type', 'color'].includes(prop),
 })<IndicatorProps>`
-    display: block;
-    background-color: ${(props) => props.theme.color.default};
-
-    ${(props) =>
-        props.type === 'primary' &&
-        css`
-            background-color: ${props.theme.color.primary.alpha};
-        `}
-
-    ${(props) =>
-        props.type === 'success' &&
-        css`
-            background-color: ${props.theme.color.success.alpha};
-        `}
-    
-  ${(props) =>
-        props.type === 'warning' &&
-        css`
-            background-color: ${props.theme.color.warning.alpha};
-        `}
-    
-  ${(props) =>
-        props.type === 'error' &&
-        css`
-            background-color: ${props.theme.color.error.alpha};
-        `}
-  
-  ${(props) =>
-        props.size === 'small' &&
-        css`
-            width: ${props.theme.padding.sm};
-            height: ${props.theme.padding.sm};
-            border-radius: ${props.theme.padding.sm};
-        `}
-    
-  ${(props) =>
-        props.size === 'medium' &&
-        css`
-            width: ${props.theme.padding.md};
-            height: ${props.theme.padding.md};
-            border-radius: ${props.theme.padding.md};
-        `}
-    
-  ${(props) =>
-        props.size === 'large' &&
-        css`
-            width: ${props.theme.padding.lg};
-            height: ${props.theme.padding.lg};
-            border-radius: ${props.theme.padding.lg};
-        `}
+    ${({ size, type }) => css`
+        ${getIndicatorDefaultStyles()}
+        ${getIndicatorDefaultTypeStyles(type as IndicatorProps['type'])}
+        ${getIndicatorDefaultSizeStyles(size as IndicatorProps['size'])}
+    `}
 `;
