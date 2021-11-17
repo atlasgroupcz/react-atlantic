e5f01ff843a17b42229c6fbe41570cf3b69af1f1
nikolasrnickova <nikola.srnickova@gmail.com>
2021-11-17 10:16:20 +0100

    chore: 🔧 remake

--- src/gatsby-theme-docz/components/Wrapper/views/Horizontal/Horizontal.style.ts
+++ src/gatsby-theme-docz/components/Wrapper/views/Horizontal/Horizontal.style.ts
@@ -3,5 +3,6 @@ import { styled } from '../../../../../styled';
 export const StyledWrapperHorizontal = styled.div`
     display: flex;
     align-items: center;
+    flex-wrap: wrap;
     column-gap: 5px;
 `;
