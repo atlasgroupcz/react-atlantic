cb1d63738b86d77953e01ff13d05d53cbf8a2424
Martin Topolanek <topolanekmar@gmail.com>
2022-10-24 13:49:38 +0200

    chore(button): 🔧 component can now pass ref prop down to element

--- src/components/Button/view/default/Button.tsx
+++ src/components/Button/view/default/Button.tsx
@@ -1,8 +1,8 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, PropsWithChildren } from 'react';
 import { StyledButton } from './styles';
 import { ButtonProps } from '../../types';
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>(
     ({ htmlType, type = 'default', className, ...props }, ref) => {
         return <StyledButton atlanticType={type} type={htmlType} className={className} {...props} ref={ref} />;
     }
