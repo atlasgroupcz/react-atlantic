c15991a50cc189e4f864fab5bb8de59650c4f7c7
Martin Topolánek <topolanekmar@gmail.com>
2023-03-07 12:00:14 +0100

    chore: 🔧 checkbox as forward ref

--- src/components/Checkbox/view/alpha/Checkbox.tsx
+++ src/components/Checkbox/view/alpha/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef, PropsWithChildren } from 'react';
 import {
     HiddenCheckbox,
     StyledCheckboxInputShown,
@@ -6,66 +6,68 @@ import {
     StyledCheckboxMark,
     StyledCheckboxSpan,
 } from './styles';
-import { CheckboxType } from '../../types';
 import { Icon } from '../../../Icon';
+import { CheckboxProps } from '../../types';
 
-export const Checkbox: CheckboxType = ({
-    isPartiallyChecked,
-    horizontalPosition = 'left',
-    isDisabled,
-    children,
-    id,
-    isChecked,
-    className,
-    size = 'small',
-    ...props
-}) => (
-    <StyledCheckboxLabel
-        isChecked={isChecked}
-        isDisabled={isDisabled}
-        className={className}
-        htmlFor={id}
-        size={size}
-        data-is-disabled={isDisabled}
-        data-is-checked={isChecked}
-        data-size={size}
-    >
-        {horizontalPosition === 'right' && children && (
-            <StyledCheckboxSpan
-                position={horizontalPosition}
-                isDisabled={isDisabled}
-                size={size}
-                data-is-disabled={isDisabled}
-                data-size={size}
-            >
-                {children}
-            </StyledCheckboxSpan>
-        )}
+export const Checkbox = forwardRef<HTMLDivElement, PropsWithChildren<CheckboxProps>>(
+    ({
+        isPartiallyChecked,
+        horizontalPosition = 'left',
+        isDisabled,
+        children,
+        id,
+        isChecked,
+        className,
+        size = 'small',
+        ...props
+    }) => (
+        <StyledCheckboxLabel
+            isChecked={isChecked}
+            isDisabled={isDisabled}
+            className={className}
+            htmlFor={id}
+            size={size}
+            data-is-disabled={isDisabled}
+            data-is-checked={isChecked}
+            data-size={size}
+        >
+            {horizontalPosition === 'right' && children && (
+                <StyledCheckboxSpan
+                    position={horizontalPosition}
+                    isDisabled={isDisabled}
+                    size={size}
+                    data-is-disabled={isDisabled}
+                    data-size={size}
+                >
+                    {children}
+                </StyledCheckboxSpan>
+            )}
 
-        <HiddenCheckbox id={id} readOnly defaultChecked={isChecked} disabled={isDisabled} {...props} />
-        <StyledCheckboxInputShown isDisabled={isDisabled} data-is-disabled={isDisabled}>
-            <StyledCheckboxMark
-                isDisabled={isDisabled}
-                isChecked={isChecked}
-                isPartiallyChecked={isPartiallyChecked}
-                data-is-disabled={isDisabled}
-                data-is-checked={isChecked}
-                data-is-partially-checked={isPartiallyChecked}
-            >
-                <Icon name="check" />
-            </StyledCheckboxMark>
-        </StyledCheckboxInputShown>
+            <HiddenCheckbox id={id} readOnly defaultChecked={isChecked} disabled={isDisabled} {...props} />
+            <StyledCheckboxInputShown isDisabled={isDisabled} data-is-disabled={isDisabled}>
+                <StyledCheckboxMark
+                    isDisabled={isDisabled}
+                    isChecked={isChecked}
+                    isPartiallyChecked={isPartiallyChecked}
+                    data-is-disabled={isDisabled}
+                    data-is-checked={isChecked}
+                    data-is-partially-checked={isPartiallyChecked}
+                >
+                    <Icon name="check" />
+                </StyledCheckboxMark>
+            </StyledCheckboxInputShown>
 
-        {horizontalPosition === 'left' && children && (
-            <StyledCheckboxSpan
-                position={horizontalPosition}
-                isDisabled={isDisabled}
-                size={size}
-                data-is-disabled={isDisabled}
-                data-size={size}
-            >
-                {children}
-            </StyledCheckboxSpan>
-        )}
-    </StyledCheckboxLabel>
+            {horizontalPosition === 'left' && children && (
+                <StyledCheckboxSpan
+                    position={horizontalPosition}
+                    isDisabled={isDisabled}
+                    size={size}
+                    data-is-disabled={isDisabled}
+                    data-size={size}
+                >
+                    {children}
+                </StyledCheckboxSpan>
+            )}
+        </StyledCheckboxLabel>
+    )
 );
